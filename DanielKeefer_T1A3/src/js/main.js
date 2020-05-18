c16db6d2dd213b8dfd8549c8c0ac9500d772d190
xxKeefer <xxkeefer.code@gmail.com>
2020-05-18 13:59:22 +1000

    added skills page and associated elements

--- DanielKeefer_T1A3/src/js/main.js
+++ DanielKeefer_T1A3/src/js/main.js
@@ -2,7 +2,6 @@
 const menuButton = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
 //menu default start
@@ -15,7 +14,6 @@ function toggleMenu() {
     menuButton.classList.add('close');
     menu.classList.add('show');
     menuNav.classList.add('show');
-    menuBranding.classList.add('show');
     navItems.forEach(item => item.classList.add('show'));
 
     //toggle menu show state
@@ -25,7 +23,6 @@ function toggleMenu() {
     menuButton.classList.remove('close');
     menu.classList.remove('show');
     menuNav.classList.remove('show');
-    menuBranding.classList.remove('show');
     navItems.forEach(item => item.classList.remove('show'));
 
     //toggle menu show state
